2a3d39ab34e50b2baa8f2016c658875ddf9b8f0a
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2022-10-03 01:18:26 -0400

    Create Q55.py

---
+++ Q55.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal_post = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal_post:
+                goal_post = i
+
+        return (goal_post == 0)
